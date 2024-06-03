14eaa025a6d8bb34e38630d0f6a470c5d489da3f
Aslam Khan shah <aksi34012@gmail.com>
2024-06-03 12:25:50 +0530

    done

---
+++ main.rs
@@ -0,0 +1,15 @@
+
+
+fn main() {
+    let number = 10;
+     
+     if number > 5 {
+         println!("number is greater than 5");
+     }
+     else if number == 5 {
+         println!("number is equal to 5");
+     }
+     else {
+         println!("number is less than 5");
+     }
+}
